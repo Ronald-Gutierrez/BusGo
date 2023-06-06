6ba6ab3a670a1f328b5059fa8413866f1b0b72c8
UbertoGC <ugarcia@unsa.edu.pe>
2023-06-06 10:59:36 -0500

    Algunas pequeñas correcciones y modificaciones.

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\HomeController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('auth/login');
+})->name('login');
 
 Auth::routes();
 
