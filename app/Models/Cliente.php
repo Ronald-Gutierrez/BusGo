c0f9e07a2a76bae10c5264a777443aa3ebcf7d5d
UbertoGC <ugarcia@unsa.edu.pe>
2023-06-28 22:23:46 -0500

    Actualizando modelos para tablas de usuario

---
+++ app/Models/Cliente.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+/**
+ * Class Cliente
+ *
+ * @property $id_usuario
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Cliente extends Model
+{
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+      'id_usuario',
+    ];
+}
