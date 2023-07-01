f168bc6cfdcc7943d118cff2ae931c1e06fcac09
Ronald Romario Gutierrez Arratia <113565299+Ronald-Gutierrez@users.noreply.github.com>
2023-07-01 17:57:42 -0500

    Actualizacion del main

--- app/Http/Controllers/SelectViajeController.php
+++ app/Http/Controllers/SelectViajeController.php
@@ -1,17 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\Bus;
+
 use Illuminate\Http\Request;
 
 class SelectviajeController extends Controller
 {
-    public function index($id_viaje)
+    public function index()
     {
-        $bus = Bus::where('id_viaje',$id_viaje)->paginate();
-        $datos = $bus[0];
-        $estados = $datos["asientos"];
-        
-        return view('selectviaje',compact('estados'));
+        return view('selectviaje'); 
     }
 }
